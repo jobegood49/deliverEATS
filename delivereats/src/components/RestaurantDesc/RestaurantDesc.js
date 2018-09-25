e7874678828e988cf6fab6555ee14d5996e9e6d8
jonathannicolas <jonathan.s.nicolas@gmail.com>
2018-09-24 18:14:47 -0700

    add style to restaurant desc

--- delivereats/src/components/RestaurantDesc/RestaurantDesc.js
+++ delivereats/src/components/RestaurantDesc/RestaurantDesc.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './RestaurantDesc.css'
 
 const RestaurantDesc = ({ restaurant }) => {
   return (
-    <div>
-      <p>{restaurant.name}</p>
-      <p>{restaurant.description}</p>
-      <img src={restaurant.picture}/>
+    <div className="restaurantDescContainer">
+      <div className="restaurantInfo">
+        <p>{restaurant.name}</p>
+        <p>{restaurant.description}</p>
+      </div>
+      <div className="restaurantImage">
+        <img src={restaurant.picture} />
+      </div>
     </div>
   );
 };
