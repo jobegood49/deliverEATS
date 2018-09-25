5769fdfe2a8c982dc3fbde438ac03398fe6351ca
jonathannicolas <jonathan.s.nicolas@gmail.com>
2018-09-24 17:15:19 -0700

    Add header component

--- delivereats/src/App.js
+++ delivereats/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Menus from './components/Menus';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/menus" component={Menus} />
+        <div>
+          <Header />
+          <Route path="/menus" component={Menus} />
+        </div>
       </Router>
     );
   }
---
+++ delivereats/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='headerContainer'>
+            <p>deliver eats</p>
+        </div>
+    );
+};
+
+export default Header;
