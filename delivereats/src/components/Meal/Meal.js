422cfaa908ab6d1c08fe84bd4e33e733a89d510d
justin <justinvyang@gmail.com>
2018-09-25 19:29:33 -0700

    add styles to Menus

--- delivereats/src/components/Meal/Meal.js
+++ delivereats/src/components/Meal/Meal.js
@@ -5,10 +5,14 @@ import LinesEllipsis from 'react-lines-ellipsis';
 const Meal = ({ item }) => {
   return (
     <div className="cardContainer">
-      <p>{item.title}</p>
-      <p>{item.description}</p>
-      <p>{item.price}</p>
-      <img style={{ height: 50 }} src={item.picture} />
+      <div className="itemDesc">
+        <p>{item.title}</p>
+        <p>{item.description}</p>
+        <p>{item.price}</p>
+      </div>
+      <div className="itemPic">
+        <img style={{ height: 50 }} src={item.picture} />
+      </div>
     </div>
   );
 };
