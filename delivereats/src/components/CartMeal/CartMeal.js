9ff7d1bef701cdca2eb21dbc0db48d556327a96a
jonathannicolas <jonathan.s.nicolas@gmail.com>
2018-09-28 17:44:29 -0700

    Add styling to button increment decrement

--- delivereats/src/components/CartMeal/CartMeal.js
+++ delivereats/src/components/CartMeal/CartMeal.js
@@ -5,9 +5,13 @@ const CartMeal = ({ cartMeal, increment, decrement }) => {
   return (
     <div className="cartMealContainer">
       <div className="quantityContainer">
-        <button onClick={() => decrement(cartMeal.id)}>-</button>
+        <button className="cart-button" onClick={() => decrement(cartMeal.id)}>
+          <i className="fas fa-minus-circle" />
+        </button>
         <p>{cartMeal.quantity}</p>
-        <button onClick={() => increment(cartMeal.id)}>+</button>
+        <button className="cart-button" onClick={() => increment(cartMeal.id)}>
+          <i className="fas fa-plus-circle" />
+        </button>
         {cartMeal.title}
       </div>
       <p>{(cartMeal.price * cartMeal.quantity).toFixed(2)}</p>
